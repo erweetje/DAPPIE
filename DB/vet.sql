5888c58276471db311a0c80f08a9ae93b1e85d7a
Sarah Heindryckx <sarah.heindryckx@hotmail.com>
2015-07-02 13:15:52 +0200

    eerste poging om entities e.d. op github te plaatsen

---
+++ DB/vet.sql
@@ -0,0 +1,86 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                5.6.21 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Versie:              9.2.0.4947
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Databasestructuur van vet wordt geschreven
+CREATE DATABASE IF NOT EXISTS `vet` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `vet`;
+
+
+-- Structuur van  tabel vet.consultatie wordt geschreven
+CREATE TABLE IF NOT EXISTS `consultatie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gewicht` decimal(10,0) NOT NULL DEFAULT '0',
+  `dierId` int(11) NOT NULL DEFAULT '0',
+  `datum` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `notitie` text,
+  PRIMARY KEY (`id`),
+  KEY `FK_Consultatie_Dier` (`dierId`),
+  CONSTRAINT `FK_Consultatie_Dier` FOREIGN KEY (`dierId`) REFERENCES `dier` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Dumpen data van tabel vet.consultatie: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `consultatie` DISABLE KEYS */;
+INSERT INTO `consultatie` (`id`, `gewicht`, `dierId`, `datum`, `notitie`) VALUES
+	(1, 10, 6, '0000-00-00 00:00:00', 'Pretty little puggy');
+/*!40000 ALTER TABLE `consultatie` ENABLE KEYS */;
+
+
+-- Structuur van  tabel vet.dier wordt geschreven
+CREATE TABLE IF NOT EXISTS `dier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(50) NOT NULL DEFAULT '0',
+  `stamboomnaam` varchar(100) NOT NULL DEFAULT 'Geen stamboomnaam',
+  `soort` varchar(50) NOT NULL DEFAULT '0',
+  `ras` varchar(50) NOT NULL DEFAULT '0',
+  `geboortedatum` date NOT NULL,
+  `chipnummer` varchar(50) NOT NULL DEFAULT '0',
+  `paspoortnummer` varchar(50) NOT NULL DEFAULT '0',
+  `kleur` text NOT NULL,
+  `medbeeld` varchar(100) NOT NULL,
+  `klantId` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_Klant_Dier` (`klantId`),
+  CONSTRAINT `FK_Klant_Dier` FOREIGN KEY (`klantId`) REFERENCES `klant` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- Dumpen data van tabel vet.dier: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `dier` DISABLE KEYS */;
+INSERT INTO `dier` (`id`, `naam`, `stamboomnaam`, `soort`, `ras`, `geboortedatum`, `chipnummer`, `paspoortnummer`, `kleur`, `medbeeld`, `klantId`) VALUES
+	(6, 'Rocky', 'St.-Hubertus', 'hond', 'Mopshond', '0000-00-00', '1', '1', 'beige', 'voorbeeld.jpg', 2);
+/*!40000 ALTER TABLE `dier` ENABLE KEYS */;
+
+
+-- Structuur van  tabel vet.klant wordt geschreven
+CREATE TABLE IF NOT EXISTS `klant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naam` varchar(50) NOT NULL DEFAULT '0',
+  `voornaam` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(255) NOT NULL DEFAULT '0',
+  `straat` varchar(100) NOT NULL DEFAULT '0',
+  `wachtwoord` varchar(100) NOT NULL DEFAULT '0',
+  `huisnummer` varchar(50) NOT NULL DEFAULT '0',
+  `postcode` char(4) NOT NULL DEFAULT '0',
+  `woonplaats` varchar(150) NOT NULL DEFAULT '0',
+  `tel` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- Dumpen data van tabel vet.klant: ~0 rows (ongeveer)
+/*!40000 ALTER TABLE `klant` DISABLE KEYS */;
+INSERT INTO `klant` (`id`, `naam`, `voornaam`, `email`, `straat`, `wachtwoord`, `huisnummer`, `postcode`, `woonplaats`, `tel`) VALUES
+	(2, 'Heindryckx', 'Sarah', 'sarah.heindryckx@hotmail.com', 'Spanjaardstraat', '123', '24', '8830', 'Hooglede', '0497455712'),
+	(3, 'ExampleLn', 'ExampleFn', 'example@hexample.com', 'Examplestreet', '123', '1', '1111', 'ExampleCity', '1111111111');
+/*!40000 ALTER TABLE `klant` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ Entities/Klant.php
@@ -0,0 +1,91 @@
+<?php
+
+class Klant {
+    private $id;
+    private $naam;
+    private $voornaam;
+    private $straat;
+    private $huisnummer;
+    private $postcode;
+    private $woonplaats;
+    private $email;
+    private $wachtwoord;
+    private $tel;
+
+    public function __construct($id, $naam, $voornaam, $straat, $huisnummer,
+            $postcode, $woonplaats, $email, $wachtwoord, $tel){
+        $this->id=$id;
+        $this->naam=$naam;
+        $this->voornaam=$voornaam;
+        $this->straat=$straat;
+        $this->huisnummer=$huisnummer;
+        $this->postcode=$postcode;
+        $this->woonplaats=$woonplaats;
+        $this->email=$email;
+        $this->wachtwoord=$wachtwoord;
+        $this->tel=$tel;
+        
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+    public function getNaam() {
+        return $this->naam;
+    }
+    public function getVoornaam() {
+        return $this->voornaam;
+    }
+    public function getStraat() {
+        return $this->straat;
+    }
+    public function getHuisnummer() {
+        return $this->huisnummer;
+    }
+            
+    public function getPostcode() {
+        return $this->postcode;
+    }
+    public function getWoonplaats() {
+        return $this->woonplaats;
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+    public function getWachtwoord() {
+        return $this->wachtwoord;
+    }
+    
+    public function getTel() {
+        return $this->tel;
+    }
+    
+    public function setNaam($naam) {
+        $this->naam=$naam;
+    }
+    public function setVoornaam($voornaam) {
+        $this->voornaam=$voornaam;
+    }
+    public function setStraat($straat) {
+        $this->straat=$straat;
+    }
+    public function setHuisnummer($huisnummer) {
+        $this->huisnummer=$huisnummer;
+    }
+    public function setPostcode($postcode) {
+        $this->postcode=$postcode;
+    }
+    public function setWoonplaats($woonplaats) {
+        $this->woonplaats=$woonplaats;
+    }
+    public function setEmail($email) {
+        $this->email=$email;
+    }
+    public function setWachtwoord($wachtwoord) {
+        $this->wachtwoord=$wachtwoord;
+    }
+    public function setTel($tel) {
+        $this->tel=$tel;
+    }    
+           
+}
